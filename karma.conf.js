bf8eba0628b0b3ed0bbc69449f6e92bb44e339ea
Daniel Suess <daniel.suess@akquinet.de>
2016-12-22 14:14:04 +0100

    setup test infrastructure (add node dependencies)

---
+++ karma.conf.js
@@ -0,0 +1,28 @@
+module.exports = function(config) {
+    config.set({
+        basePath: '../..',
+        frameworks: ['jasmine'],
+        //...
+        logLevel: config.LOG_INFO,
+        //logLevel: config.LOG_DEBUG,
+        //browsers: ["Chrome"],
+        browsers: ["PhantomJS2"],
+        basePath: ".",
+        files: [
+            "bower_components/angular/angular.js",
+            //"bower_components/**/*.js",
+            "node_modules/angular-mocks/angular-mocks.js",
+            "js/services.js",
+            //"js/*.*",
+            //"lib/*.*",
+            //"charts/*.*",
+            //"data/*.*",
+            //"partials/*.*",
+            //"templates/*.*",
+
+            "tests/*.*"
+        ],
+        autoWatch: true,
+        autoWatchBatchDelay: 250 //default
+    });
+};
